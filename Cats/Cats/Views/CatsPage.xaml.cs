9c537fa2e9ba3b598f4a902bc59c7f98a11c6750
Alex Almeida <alex.aldr@gmail.com>
2017-03-31 11:37:57 -0300

    Added Page Details
    Added Page Details

--- Cats/Cats/Views/CatsPage.xaml.cs
+++ Cats/Cats/Views/CatsPage.xaml.cs
@@ -20,47 +20,57 @@ namespace Cats.Views
         public CatsPage()
         {
             InitializeComponent();
+            ListViewCats.ItemSelected += ListViewCats_ItemSelected;
         }
+
+        private async void ListViewCats_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var SelectedCat = e.SelectedItem as Models.Cat;
+            if (SelectedCat != null)
+            {
+                await Navigation.PushAsync(new Views.DetailsPage(SelectedCat));
+                ListViewCats.SelectedItem = null;
+            }
+        }
+
+
+        /*
+        [XamlCompilation(XamlCompilationOptions.Compile)]
+        public partial class CatsPage : ContentPage
+        {
+            public CatsPage()
+            {
+                InitializeComponent();
+                BindingContext = new CatsPageViewModel(); //this; // new ContentPage();
+            }
+        }
+
+        class CatsPageViewModel : INotifyPropertyChanged
+        {
+
+            public CatsPageViewModel()
+            {
+                IncreaseCountCommand = new Command(IncreaseCount);
+            }
+
+            int count;
+
+            string countDisplay = "You clicked 0 times.";
+            public string CountDisplay
+            {
+                get { return countDisplay; }
+                set { countDisplay = value; OnPropertyChanged(); }
+            }
+
+            public ICommand IncreaseCountCommand { get; }
+
+            void IncreaseCount() =>
+                CountDisplay = $"You clicked {++count} times";
+
+
+            public event PropertyChangedEventHandler PropertyChanged;
+            void OnPropertyChanged([CallerMemberName]string propertyName = "") =>
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+                */
     }
-
-
-    /*
-    [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class CatsPage : ContentPage
-    {
-        public CatsPage()
-        {
-            InitializeComponent();
-            BindingContext = new CatsPageViewModel(); //this; // new ContentPage();
-        }
-    }
-
-    class CatsPageViewModel : INotifyPropertyChanged
-    {
-
-        public CatsPageViewModel()
-        {
-            IncreaseCountCommand = new Command(IncreaseCount);
-        }
-
-        int count;
-
-        string countDisplay = "You clicked 0 times.";
-        public string CountDisplay
-        {
-            get { return countDisplay; }
-            set { countDisplay = value; OnPropertyChanged(); }
-        }
-
-        public ICommand IncreaseCountCommand { get; }
-
-        void IncreaseCount() =>
-            CountDisplay = $"You clicked {++count} times";
-
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        void OnPropertyChanged([CallerMemberName]string propertyName = "") =>
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-
-    }*/
 }
